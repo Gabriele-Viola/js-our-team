994c6da2c23fd7e1e8176e6e6e75af867c152527
Gabriele-Viola <gabrieleviola50@gmail.com>
2024-10-14 16:04:18 +0200

    add rowEl

--- assets/script.js
+++ assets/script.js
@@ -37,3 +37,27 @@ const teamMembers = [
   }
 ];
 
+const rowEl = document.getElementById('row')
+console.log(rowEl);
+
+let member 
+for (let i = 0; i < teamMembers.length; i++) {
+  const element = teamMembers[i];
+  console.log(member);  
+}
+const {name, role, email, img} = member
+console.log(name, role, email);
+
+
+rowEl.innerHTML = `                <div class="col">
+                    <div class="d-flex gap-3 my-2 bg-dark co">
+                        <img class="col-4" src="./assets/${img}" alt="">
+                        <div class="col-7 text-light">
+                            <h3 class="text-light">${name}</h3>
+                            <p>${role}</p>
+                            <p class="text-primary">${email}</p>
+                        </div>
+                    </div>
+                </div>`
+
+
